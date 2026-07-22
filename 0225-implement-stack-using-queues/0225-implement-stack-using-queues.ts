359d3826fdd47d4f59cadebc99c78405c0477029
Misha Moskalenko <misha.moskalenko.jv@gmail.com>
2026-07-22 23:26:49 +0300

    Time: 0 ms (100%), Space: 55.6 MB (32.08%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.ts
@@ -0,0 +1,30 @@
+class MyStack {
+    private stack: number[] = [];
+
+    constructor() {}
+
+    push(x: number): void {
+        this.stack.push(x);
+    }
+
+    pop(): number {
+        return this.stack.pop();
+    }
+
+    top(): number {
+        return this.stack.at(-1);
+    }
+
+    empty(): boolean {
+        return this.stack.length === 0;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
