4f85d7bd0590e7fd72037ba8e5c435867c680b45
Misha Moskalenko <misha.moskalenko.jv@gmail.com>
2026-07-27 23:01:27 +0300

    Time: 8 ms (46.69%), Space: 65.3 MB (45.53%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.ts
+++ 0009-palindrome-number/0009-palindrome-number.ts
@@ -5,14 +5,10 @@ function isPalindrome(x: number): boolean {
     if (s.length % 2 === 0) {
         left = s.length / 2 - 1;
         right = s.length / 2;
-        console.log("s.length % 2 === 0")
     } else {
         left = Math.floor(s.length / 2);
         right = Math.floor(s.length / 2);
-        console.log("s.length % 2 !== 0")
     }
-
-    console.log(left, right)
 
     while (left >= 0 && right <= s.length) {
         if (s[left] !== s[right]) return false;
