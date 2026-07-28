110832a04a0e770c017892a6f42cde8060c0e801
Misha Moskalenko <misha.moskalenko.jv@gmail.com>
2026-07-28 09:23:32 +0300

    Time: 0 ms (100%), Space: 60.3 MB (21.92%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.ts
+++ 1929-concatenation-of-array/1929-concatenation-of-array.ts
@@ -1,3 +1,7 @@
 function getConcatenation(nums: number[]): number[] {
-    return nums.concat(nums);
+    const len = nums.length;
+    for (let i = 0; i < len; i++) {
+        nums.push(nums[i])
+    }
+    return nums;
 };
