6027c3c7b5d18c1160b5a30721c721ad727bdab6
Misha Moskalenko <misha.moskalenko.jv@gmail.com>
2026-07-29 08:30:14 +0300

    Time: 2 ms (46.81%), Space: 56.7 MB (38.3%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.ts
@@ -0,0 +1,27 @@
+function calPoints(operations: string[]): number {
+    let resTrack = [];
+    let res = 0;
+
+    for (let i = 0; i < operations.length; i++) {
+        if (operations[i] === '+') {
+            const n = resTrack.at(-1) + resTrack.at(-2);
+            res += n;
+            resTrack.push(n);
+        }
+        else if (operations[i] === 'D') {
+            const n = resTrack.at(-1) * 2;
+            res += n;
+            resTrack.push(n);
+        }
+        else if (operations[i] === 'C') {
+            res -= resTrack.pop();
+        }
+        else {
+            const n = +operations[i];
+            res += n;
+            resTrack.push(n);
+        }
+    }
+
+    return res;
+};
